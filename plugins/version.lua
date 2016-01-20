87e4b5c2e4b3c433ebe272a0e2133134fb366759
testprmagma <hackehh@30mail.ir>
2016-01-20 21:02:10 +0330

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+  return 'Telebot '.. VERSION .. [[ 
+  default source by seed and edit by Unkownhacker
+  Admins:
+ @Unkownhacker
+end
+return {
+    patterns = {
+    "^[!/]([Vv]ersion)$"
+  }, 
+  run = run 
+}
+end
