12fdd9627b9e39fb98435ee72fac817099fa2b2a
testprmagma <hackehh@30mail.ir>
2016-01-20 20:44:51 +0330

    Create autoleave.lua

---
+++ plugins/autoleave.lua
@@ -0,0 +1,29 @@
+--[[
+Kicking ourself (bot) from unmanaged groups.
+When someone invited this bot to a group, the bot then will chek if the group is in its moderations (moderation.json). If not, the bot will exit immediately by kicking itself out of that group.
+No switch available. You need to turn it on or off using !plugins command.
+Testing needed.
+--]]
+
+-- suppress '*** lua: attempt to call a nil value' warning
+local function callback(extra, success, result)
+  vardump(success)
+  vardump(result)
+end
+
+local function run(msg)
+  if msg.service and msg.action.type == 'chat_add_user' then
+    local data = load_data(_config.moderation.data)
+    if not data[tostring(msg.to.id)] then
+      print "میخواستی کیرت کنم😐؟الان به ارزوت میرسونمت😆"
+      chat_del_user('chat#id'..msg.to.id, 'user#id'..our_id, callback, false)
+    end
+  end
+end
+
+return {
+    patterns = {
+    "^!!tgservice (.+)$"
+  },
+  run = run
+}
